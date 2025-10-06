a37667a157a8459dffecae83f60d6b30fea7279f
Isaias Costa <isaiascssilva@gmail.com>
2025-10-06 14:18:21 -0300

    correção de apontamento

--- src/main/java/com/isaiascosta/agendadortarefas/infrastructure/client/UsuarioClient.java
+++ src/main/java/com/isaiascosta/agendadortarefas/infrastructure/client/UsuarioClient.java
@@ -2,14 +2,13 @@ package com.isaiascosta.agendadortarefas.infrastructure.client;
 
 import com.isaiascosta.agendadortarefas.infrastructure.business.dto.UsuarioDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "usuario", url="${usuario.url}")
 public interface UsuarioClient {
-   @GetMapping("/por-email")
+   @GetMapping("/usuario")
    UsuarioDTO buscarPorEmail(@RequestParam("email") String email,
                              @RequestHeader("Authorization") String token);
 }
