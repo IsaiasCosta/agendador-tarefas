7554bb161b70fec4a24f93c9a1d078df6cdad1a4
Isaias Costa <isaiascssilva@gmail.com>
2025-10-06 10:51:10 -0300

    commit inicial

---
+++ src/test/java/com/isaiascosta/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaiascosta.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
