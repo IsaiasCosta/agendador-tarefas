d36eb16df9d9e6eae3d0f8d188e5d88c6205a3d1
Isaias Costa <isaiascssilva@gmail.com>
2025-11-05 21:43:29 -0300

    add dockfile e alterações

---
+++ src/main/java/com/isaiascosta/agendadortarefas/infrastructure/business/dto/TarefasDTORecord.java
@@ -0,0 +1,18 @@
+package com.isaiascosta.agendadortarefas.infrastructure.business.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.isaiascosta.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
+
+import java.time.LocalDateTime;
+
+public record TarefasDTORecord(String id,
+                               String nomeTarefa,
+                               String descricao,
+                               @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+                               LocalDateTime dataCriacao,
+                               @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+                               LocalDateTime dataEvento,
+                               String emailUsuario,
+                               @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+                               LocalDateTime dataAlteracao,
+                               StatusNotificacaoEnum status) {}
--- src/main/java/com/isaiascosta/agendadortarefas/infrastructure/business/mapper/TarefaUpdateConverter.java
+++ src/main/java/com/isaiascosta/agendadortarefas/infrastructure/business/mapper/TarefaUpdateConverter.java
@@ -1,6 +1,6 @@
 package com.isaiascosta.agendadortarefas.infrastructure.business.mapper;
 
-import com.isaiascosta.agendadortarefas.infrastructure.business.dto.TarefasDTO;
+import com.isaiascosta.agendadortarefas.infrastructure.business.dto.TarefasDTORecord;
 import com.isaiascosta.agendadortarefas.infrastructure.entity.TarefasEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
@@ -9,5 +9,5 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface TarefaUpdateConverter {
 
-   void updateTarefas(TarefasDTO dto, @MappingTarget TarefasEntity entity);
+   void updateTarefas(TarefasDTORecord dto, @MappingTarget TarefasEntity entity);
 }
