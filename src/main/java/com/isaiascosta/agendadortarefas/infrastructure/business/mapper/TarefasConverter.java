6bdbff6186d9be8416e1f04b6694ef5a59d753d3
Isaias Costa <isaiascssilva@gmail.com>
2025-11-08 14:30:47 -0300

    atualiza security

--- src/main/java/com/isaiascosta/agendadortarefas/infrastructure/business/mapper/TarefasConverter.java
+++ src/main/java/com/isaiascosta/agendadortarefas/infrastructure/business/mapper/TarefasConverter.java
@@ -10,10 +10,13 @@ import java.util.List;
 @Mapper(componentModel = "spring") // Indica que o MapStruct gerará uma implementação gerenciada pelo Spring
 public interface TarefasConverter {
    @Mapping(source = "id", target = "id")
-   @Mapping(source ="dataEvento", target = "dataEvento")
-   @Mapping(source ="dataCriacao", target = "dataCriacao")
+   @Mapping(source = "dataEvento", target = "dataEvento")
+   @Mapping(source = "dataCriacao", target = "dataCriacao")
    TarefasEntity paraTarefaEntity(TarefasDTORecord dto); // Converte um objeto DTO em uma entidade para persistência
+
    TarefasDTORecord paraTarefaDTO(TarefasEntity entity); // Converte uma entidade em DTO para retorno ao cliente
+
    List<TarefasEntity> paraListaTarefasEntity(List<TarefasDTORecord> dtos);
-   List<TarefasDTORecord>paraListaTarefasDTORecord(List<TarefasEntity>entities);
+
+   List<TarefasDTORecord> paraListaTarefasDTORecord(List<TarefasEntity> entities);
 }
